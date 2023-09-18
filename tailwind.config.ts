a38e9763c1ee093e731ea79e65758e1eb23188ad
zrvnarmarin <76marin76@gmail.com>
2023-09-19 01:24:59 +0200

    project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '420px',
+        'sm': '640px',     // Small screens, such as mobile phones (up to 640px)
+        'md': '768px',     // Medium screens, such as tablets (between 640px and 767px)
+        'lg': '1024px',    // Large screens, such as laptops (between 768px and 1023px)
+        'xl': '1280px',    // Extra-large screens, such as desktops (between 1024px and 1279px)
+        '2xl': '1440px',   // Larger desktop screens (between 1280px and 1535px)
+        '3xl': '1920px'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
