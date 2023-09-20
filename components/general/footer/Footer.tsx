5900570af81aab312f9174c2e343b44ab513aa60
zrvnarmarin <76marin76@gmail.com>
2023-09-21 01:38:23 +0200

    further footer styling

--- components/general/footer/Footer.tsx
+++ components/general/footer/Footer.tsx
@@ -5,38 +5,39 @@ import Link from "next/link"
 export default function Footer() {
     return (
         <footer className="px-4 sm:px-16 md:px-24 lg:px-16 xl:px-12 2xl:px-32 3xl:px-64 bg-[#F7FCFD]">
-            <section className="grid grid-cols-1 bg-red-500 px-8">
-                <div>
+            <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-16 px-8 py-12">
+                <div className="flex flex-col gap-4">
                     <Logo />
-                    <p>1429 Something Bridge, LA 4281</p>
-                    <p>Call: (321) 428 321 3902</p>
+                    <p className="pt-2 text-[#67787C] font-medium">1429 Something Bridge, LA 4281</p>
+                    <p className="text-[#062026] font-medium pb-2">Call: (321) 428 321 3902</p>
                     <div className="flex flex-row items-center gap-4">
                         {socialIcons.map(icon =>
-                            <div key={icon.id} className="bg-[#E0E6E8] rounded-full p-4">
+                            <Link key={icon.id} href={icon.href} className="bg-[#E0E6E8] rounded-full p-4">
                                 {icon.icon}
-                            </div>
+                            </Link>
                         )}
                     </div>
                 </div>
-                
-                    {footerLinks.map((footerLink, idx) =>
-                        <div key={idx} className="flex flex-col gap-8">
-                            <p>{footerLink.heading}</p>
-                            <div className="flex flex-col gap-4">
-                                {footerLink.links.map(link =>
-                                    <Link key={link.id} href={link.href}>
-                                        {link.name}
-                                    </Link>
-                                )}
-                            </div>
+                {footerLinks.map((footerLink, idx) =>
+                    <div key={idx} className="flex flex-col gap-8">
+                        <h2 className="text-[#062026] font-bold text-2xl">{footerLink.heading}</h2>
+                        <div className="flex flex-col gap-5">
+                            {footerLink.links.map(link =>
+                                <Link key={link.id} href={link.href} className="text-[#67787C] font-semibold text-lg">
+                                    {link.name}
+                                </Link>
+                            )}
                         </div>
-                    )}
-                <div>
-                    <p>Subsribe</p>
-                    <p>Subscribe to get the latest news from us</p>
-                    <div>
-                        <input type="text" />
-                        <button>Subscribe</button>
+                    </div>
+                )}
+                <div className="">
+                    <p className="text-[#062026] font-bold text-2xl pb-8">Subsribe</p>
+                    <p className="text-[#67787C] font-medium text-lg pb-4">Subscribe to get the latest news from us</p>
+                    <div className="">
+                        <div className="flex flex-col sm:flex-row items-center justify-between">
+                            <input type="text" className="bg-[#E1F7FC] max-h-full" />
+                            <button className="bg-[#24BEE0] text-white rounded-full px-5 py-3">Subscribe</button>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -45,9 +46,9 @@ export default function Footer() {
 }
 
 const socialIcons = [
-    { id:1, icon: <TwitterIcon /> },
-    { id:2, icon: <FacebookIcon /> },
-    { id:3, icon: <LinkedinIcon /> },
+    { id:1, icon: <TwitterIcon />, href: 'https://www.twitter.com' },
+    { id:2, icon: <FacebookIcon />, href: 'https://www.facebook.com' },
+    { id:3, icon: <LinkedinIcon />, href: 'https://www.instagram.com' },
 ]
 
 const footerLinks = [
